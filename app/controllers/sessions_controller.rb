708f5840c8cb2e5e7f57f7d131b4f34b45242965
nakaearth <naka5313@gmail.com>
2021-01-02 12:40:59 +0900

    ユーザ登録処理追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  skip_before_action :login?, only: %i[new create destroy oauth_failure]
+
+  def new
+    redirect_to '/auth/' + (Rails.env.production? ? params[:provider] : 'developer')
+  end
+
+  def create
+    auth_params = request.env['omniauth.auth']
+    form = UserRistrationForm.new(auth_params)
+    user =
+      User.find_by(provider: form.provider, email: form.name) ||
+      UserRegistration::TwitterUserUsecase.call(
+        email: form.email,
+        name: form.name,
+        uid: form.uid,
+        nickname: form.nickname,
+        image_url: form.image_url,
+        credentials: { secret: form.secret, token: form.token }
+      )
+    @current_user_json = 
+      {
+         encrypted_user_id: Base64.encode64(user.id.to_s),
+         user: user
+      } 
+  end
+
+  def destroy
+    reset_session
+    redirect_to controller: 'top', action: 'index', notice: 'login successfully.'
+  end
+
+  def oauth_failure
+    flash[:notice] = "キャンセルしました。"
+    redirect_to "/"
+  end
+end
