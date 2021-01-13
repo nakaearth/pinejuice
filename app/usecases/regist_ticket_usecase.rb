540059c18ebfd96f0dc815b2a92f00a407cddae2
nakaearth <naka5313@gmail.com>
2021-01-14 01:15:16 +0900

    チケット登録処理を追加

--- app/usecases/regist_ticket_usecase.rb
+++ app/usecases/regist_ticket_usecase.rb
@@ -2,8 +2,8 @@
 
 class RegistTicketUsecase
   class << self
-    def execute(user: current_user, title:, description:, point:)
-      ActiveRecord::Base.transaction do
+    def execute(current_user:, title:, description:, point:)
+      ticket = ActiveRecord::Base.transaction do
         Ticket.create!(
           title: title,
           description: description,
@@ -13,6 +13,8 @@ class RegistTicketUsecase
       rescue
         raise RegistTicketError.new('チケット登録に失敗しました。', 'regist_ticket_error')
       end
+
+      # RegistrationTicketForSearchGateway.regist(ticket)
     end
   end
 end
