db345c4782dab3d4404319632c5c46d51944540d
nakaearth <naka5313@gmail.com>
2022-04-14 07:17:32 +0900

    hello apiの追加

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class HelloController < ApplicationController
+  def new
+    head :ok
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # twitter login
+  get "/hello" => "hello#index"
   get "/:provider/login"  => "sessions#new"
   get "/logout" => "sessions#destroy"
   get "/auth/twitter/callback" => "sessions#create" unless Rails.env.development?
