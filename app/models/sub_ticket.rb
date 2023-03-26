77684bde163492a52c33ae82060e824405507fc6
nakaearth <naka5313@gmail.com>
2023-03-26 23:09:04 +0900

    追加

--- app/models/sub_ticket.rb
+++ app/models/sub_ticket.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class Ticket < ApplicationRecord
+class SubTicket < ApplicationRecord
   belongs_to :user, dependent: :destroy
-  has_many :sub_tickets
+  belongs_to :ticket, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 80 }
   validates :description, presence: true, length: { maximum: 1024 }
